ab32717a64c0df135b46d70dc6c57931e5ebb4a4
fowim-net <fowim.net@gmail.com>
2025-04-19 17:30:25 +0200

    add des truc
    .js new contact

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSection(id) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(id).style.display = 'block';
+    }
